00a33c8751ecb2fa5ad787ce52cfbce5d2751954
Nistor Vlad <nistorvlad19@stud.ase.ro>
2023-11-24 11:09:30 +0200

    almost all getters and setters

--- MyProject/Source.cpp
+++ MyProject/Source.cpp
@@ -8,26 +8,25 @@ enum seatCategory { VIP, PREMIUM, GOLD, SILVER, REGULAR };
 
 class Venue {
 private:
-    bool seatAvailability;
-    char venueName[50];
-    char venueLocation[50];
-    seatCategory seatType;
-    static int totalSeatsSold;
+    bool seatAvailability; //1-if the seat is not occupied and 0- if the seat is occupied
+    char venueName[50]; //name of venue ex:Arena Nationala
+    char venueLocation[50]; // name of location ex :Bucharest
+    seatCategory seatType; 
+    static int totalSeatsSold; 
     static const int maxNoOfSeats = 63566;
     int** seatingChart; //dynamically defined field managed by a pointer
-    int test;
 public:
     // Constructor for Venue class
-    Venue(bool availability, const char* venueName, const char* venueLocation, seatCategory seatType, int totalSeatsSold, int** seatingChart) {
+    Venue(bool availability, const char* venueName, const char* venueLocation, seatCategory type, int totalSeatsSold, int** seatingChart) {
         this->seatAvailability = availability;
         strcpy_s(this->venueName, sizeof(this->venueName), venueName);
         strcpy_s(this->venueLocation, venueLocation);
-        this->seatType = seatType;
+        this->seatType = type;
         this->totalSeatsSold = totalSeatsSold;
         this->seatingChart = seatingChart;
     }
 
-    // Getters for the attributes
+    // Getters and setters for seatAvailability 
     bool getSeatAvailability() {
         return this->seatAvailability;
     }
@@ -35,7 +34,7 @@ public:
     void setSeatAvailability(bool availability) {
         this->seatAvailability = availability;
     }
-
+    // Getters and setters for venueName
     string getVenueName() {
         return this->venueName;
     }
@@ -44,6 +43,7 @@ public:
         strcpy_s(this->venueName, name.c_str());
     }
 
+    // Getters and setters for venueLocation
     string getVenueLocation() {
         return this->venueLocation;
     }
@@ -51,7 +51,7 @@ public:
     void setVenueLocation(const string& location) {
         strcpy_s(this->venueLocation, location.c_str());
     }
-
+    //Getters and setters for seatType
     seatCategory getSeatType() {
         return this->seatType;
     }
@@ -73,6 +73,8 @@ public:
         }
     }
 
+
+
     int getTotalSeatsSold() {
         return this->totalSeatsSold;
     }
@@ -93,28 +95,63 @@ int Venue::totalSeatsSold = 0;
 int main() {
     bool availability = true;
     string name = "Ghencea";
-    string location = "ssss";
+    string location = "Bucuresti";
     seatCategory type = PREMIUM;
     int totalSold = 100;
     int** seatingChart = nullptr;
     int testNo;
     // Creating an instance of Venue
-    //cin >> testNo;
-
+    
     Venue myVenue(availability, name.c_str(), location.c_str(), type, totalSold, seatingChart);
-    // Venue secondConstruct(testNo);
-     //cout << secondConstruct.getTest() << endl;
-
-    cout << "type seat availability";
+    
+    cout << "type seat availability :";
     cin >> availability;
     myVenue.setSeatAvailability(availability);
-    cout << " Seat is available :";
-
-    cout << "type venue name:";
+    if (myVenue.getSeatAvailability() == 1) {
+        cout << "seat is available"<<endl;
+    }
+    else {
+        cout << "seat is unavailable"<<endl;
+    }
+    //cout << " Seat is available :"<<myVenue.getSeatAvailability()<<endl;
+   
+    cout << "type location name :";
+    cin >> location;
+    myVenue.setVenueLocation(location);
+    cout << "Venue location :" << myVenue.getVenueLocation()<<endl;
+    
+    cout << "type venue name :";
     cin >> name;
     myVenue.setVenueName(name);
-    cout << "Venue Name: " << myVenue.getVenueName() << std::endl;
+    cout << "Venue Name : " << myVenue.getVenueName() << endl;
 
+    cout << "input seat type(0 for VIP, 1 for PREMIUM, 2 for GOLD, 3 for SILVER, 4 for REGULAR) : ";
+    int typeInput;
+    cin >> typeInput;
+
+    switch (typeInput) {
+    case 0:
+        type = seatCategory::VIP;
+        break;
+    case 1:
+        type = seatCategory::PREMIUM;
+        break;
+    case 2:
+        type = seatCategory::GOLD;
+        break;
+    case 3:
+        type = seatCategory::SILVER;
+        break;
+    case 4:
+        type = seatCategory::REGULAR;
+        break;
+    default:
+        cout << "Invalid input" << endl;
+        return 1; // Exit the program with an error code
+    }
+
+    cout << " seat type :" << myVenue.displaySeatType(type);
+    
 
 
     //cout << myVenue.getVenueName();
